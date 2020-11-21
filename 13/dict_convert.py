6faa84a97726cdeb97b694c3f3bef9ea6f84e35d
Jarno Timmermans <netletic@gmail.com>
2020-11-21 11:57:16 +0100

    refactored bitr 13

---
+++ 13/dict_convert.py
@@ -0,0 +1,25 @@
+from collections import namedtuple
+from datetime import datetime
+import json
+
+
+blog = dict(
+    name="PyBites",
+    founders=("Julian", "Bob"),
+    started=datetime(year=2016, month=12, day=19),
+    tags=["Python", "Code Challenges", "Learn by Doing"],
+    location="Spain/Australia",
+    site="https://pybit.es",
+)
+
+
+BlogPost = namedtuple("BlogPost", "name founders started tags location site")
+
+
+def dict2nt(dict_):
+    return BlogPost(**dict_)
+
+
+def nt2json(nt):
+    nt = nt._replace(started=str(nt.started))
+    return json.dumps(nt._asdict())
