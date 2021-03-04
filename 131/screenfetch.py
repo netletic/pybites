b2635310ef6504c3e7c7cedf4cff81d3412795d2
Jarno Timmermans <netletic@gmail.com>
2021-03-04 15:29:31 +0100

    bite 131 - regex

---
+++ 131/screenfetch.py
@@ -0,0 +1,55 @@
+import re
+
+
+output = """
+                                       mohh@SERENiTY
+ MMMMMMMMMMMMMMMMMMMMMMMMMmds+.        OS: Mint 19 tara
+ MMm----::-://////////////oymNMd+'     Kernel: x86_64 Linux 4.15.0-34-generic
+ MMd      /++                -sNMd:    Uptime: 1d 4m
+ MMNso/'  dMM    '.::-. .-::.' .hMN:   Packages: 2351
+ ddddMMh  dMM   :hNMNMNhNMNMNh: 'NMm   Shell: zsh 5.4.2
+     NMm  dMM  .NMN/-+MMM+-/NMN' dMM   Resolution: 1366x768
+     NMm  dMM  -MMm  'MMM   dMM. dMM   DE: Cinnamon 3.8.9
+     NMm  dMM  -MMm  'MMM   dMM. dMM   WM: Muffin
+     NMm  dMM  .mmd  'mmm   yMM. dMM   WM Theme: Linux Mint (Mint-Y)
+     NMm  dMM'  ..'   ...   ydm. dMM   GTK Theme: Mint-Y [GTK2/3]
+     hMM- +MMd/-------...-:sdds  dMM   Icon Theme: Mint-Y
+     -NMm- :hNMNNNmdddddddddy/'  dMM   Font: Noto Sans 9
+      -dMNs-''-::::-------.''    dMM   CPU: AMD A10-7400P Radeon R6, 10 Compute Cores 4C+6G @ 4x 2.5GHz [101.0°C]
+       '/dMNmy+/:-------------:/yMMM   GPU: AMD KAVERI (DRM 2.50.0 / 4.15.0-34-generic, LLVM 6.0.0)
+          ./ydNMMMMMMMMMMMMMMMMMMMMM   RAM: 1886MiB / 6915MiB
+             \.MMMMMMMMMMMMMMMMMMM    
+"""
+
+properties = [
+    "OS",
+    "Kernel",
+    "Uptime",
+    "Packages",
+    "Shell",
+    "Resolution",
+    "DE",
+    "WM",
+    "WM Theme",
+    "GTK Theme",
+    "Icon Theme",
+    "Font",
+    "CPU",
+    "GPU",
+    "RAM",
+]
+
+
+def sysinfo_scrape(output):
+    """Scrapes the output from screenfetch and returns a dictionary"""
+    specs = {}
+    specs["Name"] = output.splitlines()[1].strip().split()[-1]
+    for prop in properties:
+        match = re.search(rf"{prop}:\s(.*)", output)
+        value = match.group(1) if match else None
+        specs[prop] = value
+    return specs
+
+
+if __name__ == "__main__":
+    print(sysinfo_scrape(output))
