121e4b412379a1ac4b70dba202b6126e5d314aa3
Jarno Timmermans <netletic@gmail.com>
2021-03-11 00:26:19 +0100

    bite 124 - itertools product or combinations

---
+++ 134/twosums.py
@@ -0,0 +1,21 @@
+from itertools import product
+
+
+def two_sums(numbers, target):
+    """Finds the indexes of the two numbers that add up to target.
+
+    :param numbers: list - random unique numbers
+    :param target: int - sum of two values from numbers list
+    :return: tuple - (index1, index2) or None
+    """
+    sorted_numbers = sorted(numbers)
+    pairs = product(sorted_numbers, sorted_numbers)
+    for n1, n2 in pairs:
+        if n1 + n2 == target:
+            return numbers.index(n1), numbers.index(n2)
+    return None
+
+
+if __name__ == "__main__":
+    numbers = [3, 10, 14, 8, 15, 5, 16, 13, 9, 2]
+    print(two_sums(numbers, 30))
